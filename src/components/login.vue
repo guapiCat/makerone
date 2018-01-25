fd7bbab12d7b55ab5bce785c4e7d7a5b773f7550
hx <he9527@outlook.com>
2018-01-25 17:19:52 +0800

    跨域访问及登录接口测试

--- src/components/login.vue
+++ src/components/login.vue
@@ -25,7 +25,7 @@
         </div>
         <div class="am-form-group am-u-sm-10 am-u-sm-centered">
           <div class="am-u-sm-11 am-u-sm-centered">
-            <a href="" class="table_button am-btn am-btn-default am-radius" name="login" id="nextstep" style="margin-right: 60px;"><span style="font-size: 20px;">登录</span></a>
+            <a @click="login()" class="table_button am-btn am-btn-default am-radius" name="login" id="nextstep" style="margin-right: 60px;"><span style="font-size: 20px;">登录</span></a>
            <router-link to="/register"class="table_button am-btn am-btn-default am-radius" name="register" id="cancel"><span style="font-size: 20px;">我要注册</span></router-link>
 
           </div>
@@ -38,14 +38,41 @@
 </template>
 
 <script>
-    export default {
-        name: "login",
+  import {AXIOS} from '../http-common'
+  export default {
+    name: "login",
+    methods: {
+      login() {
+        var name = $("#phone_studentId").val();
+        var password = $("#password").val();
+        AXIOS.post('user/login', {
+            params: {
+              username: name,
+              password: password
+            }
+          })
+          .then(response => {
+            // JSON responses are automatically parsed.
+            var result = response.data.result
+            if(result == true){
+              sessionStorage.setItem("authKey", response.data.authKey);
+              alert("登录成功");
+            }else {
+              alert("登录失败");
+            }
+            location.href = "/";
+          })
+          .catch(e => {
+            this.errors.push(e)
+          })
+      }
+    },
     mounted:function () {
       $(function(){
         $(".layout-footer").css("margin-top","0")
       })
     }
-    }
+  }
 </script>
 
 <style scoped>
