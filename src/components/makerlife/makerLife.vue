7d614898a6ad39de329c211689e370b9d434ebcd
lhk <824567241@qq.comm>
2018-02-01 10:51:06 +0800

    创客生活

--- src/components/makerlife/makerLife.vue
+++ src/components/makerlife/makerLife.vue
@@ -6,9 +6,7 @@
     </div>
     <div class="classifylist">
       <ul>
-        <li class="title">创客动态</li>
-        <li class="title classify-active">活动建议</li>
-        <li class="title">美文共赏</li>
+        <li  @click="byClassify(item.type)" v-for="item,index in titletype"    class="title">{{item.name}}</li>
 
       </ul>
     </div>
@@ -17,71 +15,83 @@
     </div>
     <div class="classifylist" >
       <ul>
-        <li  class=" title classify-active">浏览量</li>
-        <li class="title">点赞量</li>
-        <li class="title">发布时间</li>
+        <li  @click="bySort(0)" class=" title">浏览量</li>
+        <li  @click="bySort(1)" class=" title">点赞量</li>
+        <li  @click="bySort(2)" class=" title">时间</li>
+
       </ul>
     </div>
   </div>
   <div class="workshow" style="width: 1200px;margin: 50px auto; ">
-    <div  class="center-box">
-      <h3>对于创客和老师来讲，如何开展创客教育</h3>
-      <div class="work-content"><p id="work-content">目前对于大多数学校开展创客教育目前对于大多数学校开展创客教育目前对于大多数学校开展创客教育目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都</p></div>
+    <div  class="center-box" v-for="item in makerlife ">
+      <h3>{{item.makerLive.liveTitle}}</h3>
+      <div class="work-content"><p id="work-content">{{item.makerLive.liveContent}}</p></div>
       <div class="conter-footer">
-        <span>点赞数：</span><img src="../../../static/img/upvote.png"/><i>12</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <span>浏览数：</span><img src="../../../static/img/browse.png"/><i>24</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>作者：<i>螯合剂</i></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <span>发布时间：<i>2016-02-10</i></span>
+        <span>点赞数：</span><img src="../../../static/img/upvote.png"/><i>{{item.makerLive.liveThumbsUpNum}}</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span>浏览数：</span><img src="../../../static/img/browse.png"/><i>{{item.makerLive.liveScanNum}}</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>作者：<i>{{item.username}}</i></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span>发布时间：<i>{{item.makerLive.createDate}}</i></span>
       </div>
       <router-link to="/worklife/detail"class="see-details"><button type="button" class="am-btn am-btn-primary am-radius">查看详情</button></router-link>
     </div>
-    <div  class="center-box">
-      <h3>对于创客和老师来讲，如何开展创客教育</h3>
-      <div class="work-content"><p id="">目前对于大多数学校开展创客教育目前对于大多数学校开展创客教育目前对于大多数学校开展创客教育目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都</p></div>
-      <div class="conter-footer"><span>点赞数：</span><img src="../../../static/img/upvote.png"/><i>12</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <span>浏览数：</span><img src="../../../static/img/browse.png"/><i>24</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>作者：<i>螯合剂</i></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <span>发布时间：<i>2016-02-10</i></span>
-      </div>
-      <a class="see-details" ><button type="button" class="am-btn am-btn-primary am-radius">查看详情</button></a>
-    </div>
-    <div  class="center-box">
-      <h3>对于创客和老师来讲，如何开展创客教育</h3>
-      <div class="work-content"><p >目前对于大多数学校开展创客教育目前对于大多数学校开展创客教育目前对于大多数学校开展创客教育目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都</p></div>
-      <div class="conter-footer"><span>点赞数：</span><img src="../../../static/img/upvote.png"/><i>12</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <span>浏览数：</span><img src="../../../static/img/browse.png"/><i>24</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>作者：<i>螯合剂</i></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <span>发布时间：<i>2016-02-10</i></span>
-      </div>
-      <a class="see-details" ><button type="button" class="am-btn am-btn-primary am-radius">查看详情</button></a>
-    </div>
-    <div  class="center-box">
-      <h3>对于创客和老师来讲，如何开展创客教育</h3>
-      <div class="work-content"><p >目前对于大多数学校开展创客教育目前对于大多数学校开展创客教育目前对于大多数学校开展创客教育目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都目前对于大多数学校开展创客教育，创客导师都创客导师都创客导师都创客导师都创客导师都</p></div>
-      <div class="conter-footer"><span>点赞数：</span><img src="../../../static/img/upvote.png"/><i>12</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <span>浏览数：</span><img src="../../../static/img/browse.png"/><i>24</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>作者：<i>螯合剂</i></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <span>发布时间：<i>2016-02-10</i></span>
-      </div>
-      <a class="see-details" ><button type="button" class="am-btn am-btn-primary am-radius">查看详情</button></a>
-    </div>
-
 
   </div>
 </div>
 
 </template>
 
-<script>
+<script type="es6">
+     import {AXIOS} from '../../http-common'
+     import{formatDate}from  '../../../static/js/data'
     export default {
         name: "maker-life",
+      data (){
+          return{
+            fileURL:"http://192.168.1.100:9000/",
+            makerlife:[],
+            titletype:[],
+            classify:0,
+            sort:0,
+
+
+          }
+      },
+
+      created:function () {
+        AXIOS.get('makerLive/makerLiveType?typeValue=liveType',{}).then(response=>{
+          this.titletype=response.data;
+        });
+        this.reqAxios(1, 1, 1, 10);
+      },
+      methods:{
+        reqAxios:function (type,sortType,pageNum, pageSize) {
+          AXIOS.get('makerLive/makerLiveShow',{
+            params:{
+              "type":type,
+              "sortType":sortType,
+              "pageNum":pageNum,
+              "pageSize":pageSize
+            }
+          }).then(response=>{
+            this.makerlife=response.data.list;
+          }).catch(response=>{
+            this.errors.push(e);
+          })
+        },
+        byClassify: function (type) {
+          this.classify= type;
+          this.reqAxios(this.classify,this.sort ,1 ,8)
+        },
+        bySort: function (type) {
+          this.sort= type;
+          this.reqAxios(this.classify,this.sort ,1 ,8)
+        }
+      },
       mounted:function () {
         $(function(){
-          $(".title").on("click",function(){
-            $(this).addClass("classify-active").siblings().removeClass("classify-active")
-          });
           $(".work-content").each(function (i) {
             var divH = $(this).height();
-
             var $p = $("#work-content", $(this)).eq(0);
             while ($p.height() > divH) {
-              console.log($p.outerHeight())
               $p.text($p.text().replace(/(\s)*([a-zA-Z0-9]+|\W)(\.\.\.)?$/, "..."));
             };
           });
@@ -92,6 +102,9 @@
 </script>
 
 <style scoped>
+  .toggle{
+    background: #FFCA57 !important;
+  }
   .classify{
     padding-left: 18%;
   }
