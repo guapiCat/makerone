0d8418da5297b3974a3bd1d4908af74a5a13429c
lhk <824567241@qq.comm>
2018-02-27 14:31:53 +0800

    添加token userid

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,17 @@
 import axios from 'axios'
 
+var token = sessionStorage.getItem("TID");
+var userId = sessionStorage.getItem("UID");
+if (typeof (token) == "undefined" || typeof (userId) == "undefined"){
+  token = '';
+  userId = '';
+}
+
 export const AXIOS = axios.create({
   baseURL: `http://192.168.0.103:8080`,
-  withCredentials:false
+  withCredentials:false,
+  headers:{
+    token: token,
+    userId: userId
+  }
 })
