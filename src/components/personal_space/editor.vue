59399e1a6769ef67e5a1f98f5ff1835e5b1fac9a
lhk <824567241@qq.comm>
2018-03-06 15:44:36 +0800

    提交

---
+++ src/components/personal_space/editor.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <script id="editor" type="text/plain"></script>
+  </div>
+</template>
+
+<script>
+  import '../../../static/ue/themes/default/css/umeditor.min.css'
+  import '../../../static/ue/umeditor.config'
+  import '../../../static/ue/umeditor.min'
+  import '../../../static/ue/lang/zh-cn/zh-cn'
+    export default {
+        name: "editor",
+        props: {
+          id: {
+          type: String
+        },
+        config: {
+          type: Object
+        }
+      } ,
+      data() {
+        return {
+          editor: null
+        }
+      },
+      mounted() {
+        //初始化UE
+        const _this = this;
+        this.editor = UM.getEditor('editor', this.config); // 初始化UE
+        this.editor.addListener("ready", function () {
+          _this.editor.setContent(_this.defaultMsg); // 确保UE加载完成后，放入内容。
+        });
+      },
+      destoryed() {
+        this.editor.destory();
+      },
+      methods:{
+        getUEContent: function(){  // 获取内容方法
+          return this.editor.getContent();
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/personal_space/uploadAlltype.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+  <div class="workshow" style="width: 1200px;margin: 50px auto;  ">
+    <div class="center-title" style="margin-bottom: 20px; color: #969696;">
+		<span> <img src="../../../static/img/icon_address.png" alt="" /><i style="margin-left: 15px;">益谷创客平台-作品展示-<span>提交作品</span></i>
+		</span>
+    </div>
+    <SubPro  v-on:firstType="firstmessage" :fileURL="fileURL" v-show="!hidden" ></SubPro>
+     <Second v-show="hidden" :fileURL="fileURL" v-on:secondType="secondmessage" ></Second>
+
+  </div>
+</div>
+  </template>
+<script>
+  import SubPro from './SubPro'
+  import Second from './Second'
+
+    export default {
+        name: "upload-alltype",
+        data(){
+          return{
+            hidden:false,//是否展示子组件内容
+          }
+       },
+         props: {
+         fileURL: {
+          type: String,
+          required: true
+        }
+      },
+      methods:{
+        firstmessage:function (firstdata,firstdata1,firsthiden) {
+          console.log(firstdata);
+          console.log(firstdata1)
+          this.hidden=firsthiden
+        },
+        secondmessage:function (seconddata,secondtype,secondhiden) {
+          console.log(seconddata);
+          console.log(secondtype);
+          this.hidden=secondhiden
+        }
+      },
+        components:{SubPro,Second}
+    }
+</script>
+
+<style scoped>
+
+</style>
